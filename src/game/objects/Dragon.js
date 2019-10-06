91f19368d13be0de383044d2edeecde943fb7844
Jimmy Pham <jipham510@gmail.com>
2019-10-06 13:46:21 -0700

    added invincibilty after getting hit

--- src/game/objects/Dragon.js
+++ src/game/objects/Dragon.js
@@ -75,13 +75,6 @@ export default class Dragon extends GameObject {
 
 
     draw(ctx, spriteSheets, camera) {
-        ctx.strokeStyle = 'red';
-        ctx.beginPath();
-        ctx.rect(this.pos.x - camera.pos.x, 
-            this.pos.y - camera.pos.y,
-            this.width, this.height);
-        ctx.stroke();
-        
         spriteSheets.get(this.status).draw( this.frame, ctx, this.pos.x - camera.pos.x, this.pos.y - camera.pos.y)
     }
 }
